eab3b6a2195374e214bbd01aba2bed3dd6e99dac
lss555 <chrisheibel7@gmail.com>
2020-09-16 16:03:24 -0600

    Spots forms and dropdown for season

--- api/models/spots.py
+++ api/models/spots.py
@@ -1,8 +1,32 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-class Mango(models.Model):
+class Spot(models.Model):
   country = models.CharField(max_length=60)
   state = models.CharField(max_length=30)
   city = models.CharField(max_length=100)
   description = models.TextField()
+  owner = models.ForeignKey(
+      get_user_model(),
+      on_delete=models.CASCADE
+    )
+  SEASON_CHOICES = [
+    ('Summer', 'Summer'),
+    ('Winter', 'Winter'),
+    ('Either', 'Either'),
+  ]
+  season = models.CharField(max_length=50, blank=True, null=True, choices=SEASON_CHOICES)
+
+  def __str__(self):
+    return f"Your ski spots info: country: '{self.country}' state: '{self.state}' season: '{self.season}' description: '{self.description}' city: '{self.city}'"
+
+  def as_dict(self):
+    """ returns dictionary version of spot models """
+    return {
+      'id': self.id,
+      'country': self.country,
+      'state': self.state,
+      'city': self.city,
+      'description': self.description,
+      'season': self.season
+    }
---
+++ api/views/spot_views.py
@@ -0,0 +1,66 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import generics, status
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user, authenticate, login, logout
+from django.middleware.csrf import get_token
+
+from ..models.spots import Spot
+from ..serializers import SpotSerializer, UserSerializer
+
+class Spots(generics.ListCreateAPIView):
+  permission_classes=(IsAuthenticated,)
+  serializer_class = SpotSerializer
+  def get(self, request):
+    """index request"""
+    spots = Spot.objects.filter(owner=request.user.id)
+    data = SpotSerializer(spots, many=True).data
+    return Response({ 'spots': data })
+
+  def post(self, request):
+    """Create request"""
+    request.data['spot']['owner'] = request.user.id
+    spot = SpotSerializer(data=request.data['spot'])
+    if spot.is_valid():
+      spot.save()
+      return Response({ 'spot': spot.data }, status=status.HTTP_201_CREATED)
+
+    return Response(spot.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SpotDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes=(IsAuthenticated,)
+  def get(self, request, pk):
+    """Show request"""
+    spot = get_object_or_404(Spot, pk=pk)
+    if not request.user.id == spot.owner.id:
+      raise PermissionDenied('Unauthorized, you do not own this spot')
+
+    data = SpotSerializer(spot).data
+    return Response({ 'spot': data })
+
+  def delete(self, request, pk):
+    """Delete request"""
+    spot = get_object_or_404(Spot, pk=pk)
+    if not request.user.id == spot.owner.id:
+      raise PermissionDenied('Unauthorized, you do not own this spot')
+
+    spot.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+  def partial_update(self, request, pk):
+    """Update Request"""
+    if request.data['spot'].get('owner', False):
+      del request.data['spot']['owner']
+    spot = get_object_or_404(Spot, pk=pk)
+    if not request.user.id == spot.owner.id:
+      raise PermissionDenied('Unauthorized, you do not own this spot')
+
+    request.data['spot']['owner'] = request.user.id
+    data = SpotSerializer(spot, data=request.data['spot'])
+    if data.is_valid():
+      data.save()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+
+    return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
